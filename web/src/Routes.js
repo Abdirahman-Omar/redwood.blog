227ca29c8e4baf9c4a0326d0a9fc9a90fbbdd7a5
Abdirahman Omar <abdirahman@lemacurbain.com>
2021-04-13 15:59:11 -0400

    ajout de postgresql dans le prisma

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,21 +1,23 @@
-import { Router, Route, Set } from '@redwoodjs/router'
+// web/src/Routes.js
+
+import { Router, Route, Set, Private } from '@redwoodjs/router'
 import BlogLayout from 'src/layouts/BlogLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
-      <Route path="/admin/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
-      <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
-      <Route path="/admin/posts" page={PostsPage} name="posts" />
-
       <Set wrap={BlogLayout}>
         <Route path="/blog-post/{id:Int}" page={BlogPostPage} name="blogPost" />
+        <Route path="/contact" page={ContactPage} name="contact" />
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
-        <Route path="/contact" page={ContactPage} name="contact" />
       </Set>
-
+      <Private unauthenticated="home">
+        <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
+        <Route path="/admin/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
+        <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
+        <Route path="/admin/posts" page={PostsPage} name="posts" />
+      </Private>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,29 +1,38 @@
-import { Link, routes } from "@redwoodjs/router";
+import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from '@redwoodjs/auth'
 
 const BlogLayout = ({ children }) => {
-    return (
-        <>
-            <header>
-                <h1>
-                <Link to={routes.home()}>Redwood Blog</Link>
-                </h1>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to={routes.about()}>About</Link>
-                        </li>
-                        <li>
-                            <Link to={routes.posts()}>Posts</Link>
-                        </li>
-                        <li>
-                            <Link to={routes.contact()}>Contact</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <main>{children}</main>
-        </>
-    );
-};
+  const { logIn, logOut, isAuthenticated, currentUser } = useAuth()
 
-export default BlogLayout;
+  return (
+    <>
+      <header>
+        <h1>
+          <Link to={routes.home()}>Redwood Blog</Link>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+            <li>
+              <Link to={routes.posts()}>Posts</Link>
+            </li>
+            <li>
+              <Link to={routes.contact()}>Contact</Link>
+            </li>
+            <li>
+              <button href="#" onClick={isAuthenticated ? logOut : logIn}>
+                {isAuthenticated ? 'Log Out' : 'Log In'}
+              </button>
+            </li>
+            {isAuthenticated && <li>{currentUser.email}</li>}
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  )
+}
+
+export default BlogLayout
